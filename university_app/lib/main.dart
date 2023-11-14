816073538694952f80fc67a5574a31d664cf7504
Maxowino <maxwellowino27@gmail.com>
2023-11-14 09:24:13 +0300

    main changes

--- university_app/lib/main.dart
+++ university_app/lib/main.dart
@@ -12,11 +12,10 @@ void main() async{
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+ 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      //Material widget takes below properties
         title: "App",
         debugShowCheckedModeBanner:false,
        home:splashscreen(),
