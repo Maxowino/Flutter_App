afe6e347d6a98c5680a0946d20d0852922614ec5
Maxowino <maxwellowino27@gmail.com>
2023-12-06 17:03:49 +0300

    student registration firebase

--- university_app/lib/screens/register.dart
+++ university_app/lib/screens/register.dart
@@ -1,226 +1,177 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
-// import 'package:university_app/models/user.dart';
 import 'package:university_app/screens/schoollogin.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
-class HomeScreen extends StatefulWidget{
+class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
-  Future<void>addDataToFirestore(
-    String email,String phone, String pass, String user
-  )async{
-    CollectionReference registerReference=FirebaseFirestore.instance.collection('Students');
-
-    await registerReference.add({'email':email,'phone':phone,'password':pass,'username':user});
-    
-  }
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool showpassword=true;
-  final formkey= GlobalKey<FormState>();
-  final TextEditingController email= TextEditingController();
-  final TextEditingController phone=TextEditingController();
-  final TextEditingController pass=TextEditingController();
-  final TextEditingController user=TextEditingController();
+  bool showPassword = true;
+  final formKey = GlobalKey<FormState>();
+  final TextEditingController email = TextEditingController();
+  final TextEditingController phone = TextEditingController();
+  final TextEditingController pass = TextEditingController();
+  final TextEditingController user = TextEditingController();
 
-  // TextEditingController emailcontroller=new TextEditingController();
-  // TextEditingController phone=new TextEditingController();
-  // TextEditingController password=new TextEditingController();
-  // TextEditingController username=new TextEditingController();
-   
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-    backgroundColor: Colors.grey,
-  
-   body:Center(
-        
-     child:Container(
-       width:250,
-        padding:const EdgeInsets.fromLTRB(0,0, 0, 0),
-        
-          child:  Form(
-            key: formkey,
-            child: Column(  
-               crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children:[
-               
-               const  Icon(
-               Icons.account_circle_sharp,
-              color: Colors.black,
-             size: 75.0,
-            //  semanticLabel: 'Text to announce in accessibility modes',
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            width: 250,
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.account_circle_sharp,
+                    color: Colors.black,
+                    size: 75.0,
+                  ),
+                  const Text('Register', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                  buildTextFormField(email, 'Enter Email', TextInputType.emailAddress, (value) {  if (value == null || value.isEmpty) {
+                      return 'Please enter  a Email';
+                  }
+                      return null;
+                   
+                  }),
+                  buildTextFormField(user, 'Enter Username', TextInputType.text, (value) {
+                      if (value == null || value.isEmpty) {
+                    return 'Please enter  a Username';
+                  }
+                      return null;
+                  
+                   
+                  }),
+                  buildTextFormField(phone, 'Enter Phone Number', TextInputType.phone, (value) {
+                     if (value == null || value.isEmpty) {
+                      return 'Please enter  a Phone Number';
+                  }
+                       return null;
+                  
+                    // Phone number validation logic
+                  }),
+                  buildTextFormField(pass, 'Enter Password', TextInputType.text, (value) {
+                      if (value == null || value.isEmpty) {
+                       return 'Please enter  a Password';
+                  }
+                       return null;
+                  
+                    // Password validation logic
+                  }, obscureText: showPassword),
+                  buildElevatedButton(),
+                  buildRichTextForLogin(),
+                ],
               ),
-              const Text('Register', style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
-              Padding( 
-               padding:EdgeInsets.fromLTRB(0,15,0,15),
-              child:TextFormField(
-               controller: email,
-               keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-              contentPadding: EdgeInsets.fromLTRB(10,0, 10,0),
-              border:  OutlineInputBorder( borderRadius: BorderRadius.circular(20)),
-                labelText: 'Enter Email',
-              
-              ),
-               validator: (value) {
-                 bool emailvalidate= RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value!);
-                if(value.isEmpty){
-                  return "Enter your Email";
-                }
-                 
-                 if (!emailvalidate){
-                  return "Enter Valid Email";
-                 }
-                 return null;
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
-              } ,
-            ),),
-          
-            Padding( 
-               padding:EdgeInsets.fromLTRB(0,15,0,15),
-                child:TextFormField(
-                  controller: user,
-                        validator: (value) {
-                 if (value == null || value.isEmpty) {
-                return 'Please enter  a Username';
-                  }
-                return null;
-                  },
-              decoration: InputDecoration(
-              contentPadding: EdgeInsets.fromLTRB(10,0, 10,0),
-              border:  OutlineInputBorder( borderRadius: BorderRadius.circular(20)),
-                labelText: 'Enter Username',
-              
-              ),
-            ),),
-          
-             Padding( 
-               padding:EdgeInsets.fromLTRB(0,15,0,15),
-                child:TextFormField(
-                  controller: phone,
-                        validator: (value) {
-                 if (value == null || value.isEmpty) {
-                return 'Please enter  your Phone Number';
-                  }
-                return null;
-                  },
-                  keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-              contentPadding: EdgeInsets.fromLTRB(10,0, 10,0),
-              border:  OutlineInputBorder( borderRadius: BorderRadius.circular(20)),
-                labelText: 'Enter Phone Number',
-              
-              ),
-            ),),
-             
-            Padding( 
-               padding:EdgeInsets.fromLTRB(0,15,0,15),
-                   child:TextFormField(
-                    controller: pass,
-                          validator: (value) {
-                 if (value == null || value.isEmpty) {
-                return 'Please enter  a password';
-                  }
-                return null;
-                  },
-                  obscureText:showpassword,
-                  decoration: InputDecoration(
-                  suffixIcon: 
-                IconButton(
-                  onPressed: (){
-                    setState(() {
-                      showpassword=!showpassword;
-                    });
-                  },
-                  icon: Icon(Icons.remove_red_eye ,
-                  ),
-                  ),
-              contentPadding: const EdgeInsets.fromLTRB(10,0, 10,0),
-              border:  OutlineInputBorder( borderRadius: BorderRadius.circular(20)),
-                labelText: 'Enter Password',
-              
-              ),
-              // validator: (value) {
-              //   if(value!.isEmpty)
-              //   {
-              //     return "Enter Password";
-              //   }
-              //   else if(pass.text.length<5)
-              //   {
-              //     return "Password Should be more than 5 characters long";
-              //   }
-              //   return null;
-              // },
-            ),),
-          
-          
-                Padding( 
-                  padding:const EdgeInsets.fromLTRB(0,25,0,15),
-            child: ElevatedButton( 
-              style: ElevatedButton.styleFrom(
-                minimumSize:const Size(180,50),
-                backgroundColor: Colors.black,
-                foregroundColor: Colors.white,
-                shape: const StadiumBorder(),
-                side: BorderSide.none
-              ),
-              child:const Text('Register'),
-              onPressed: (){
-                    if (formkey.currentState!.validate()){
-                   ScaffoldMessenger.of(context).showSnackBar( SnackBar
-                   (content:const Text('Registration Successful!'),
-                   backgroundColor: Colors.black,
-                   duration:const Duration(seconds:3),
-                   dismissDirection: DismissDirection.up,
-                   behavior: SnackBarBehavior.floating,
-                   margin: EdgeInsets.only(
+  Widget buildTextFormField(
+    TextEditingController controller,
+    String labelText,
+    TextInputType keyboardType,
+    String? Function(String?)? validator, {
+    bool obscureText = false,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+      child: TextFormField(
+        controller: controller,
+        keyboardType: keyboardType,
+        decoration: buildInputDecoration(labelText),
+        validator: validator,
+        obscureText: obscureText,
+      ),
+    );
+  }
+
+  InputDecoration buildInputDecoration(String labelText) {
+    return InputDecoration(
+      contentPadding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+      labelText: labelText,
+    );
+  }
+
+  Widget buildElevatedButton() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 25, 0, 15),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(180, 50),
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+          shape: const StadiumBorder(),
+          side: BorderSide.none,
+        ),
+        child: const Text('Register'),
+        onPressed: () async {
+          if (formKey.currentState!.validate()) {
+            try {
+              await addDataToFirestore(email.text, phone.text, pass.text, user.text);
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text('Registration Successful!'),
+                  backgroundColor: Colors.black,
+                  duration: const Duration(seconds: 3),
+                  dismissDirection: DismissDirection.up,
+                  behavior: SnackBarBehavior.floating,
+                  margin: EdgeInsets.only(
                     bottom: MediaQuery.of(context).size.height - 120,
                     left: 40,
-                    right:40,
-                   ),
-                   
-                  ));
-                    }
-              },
-          
-              )),
-               Padding( 
-                  padding:const EdgeInsets.fromLTRB(0,15,0,15),
-                   child:RichText(
-            text:  TextSpan(
-              children:[
-                  const TextSpan(
-                    text: "Already Have An Account? ",
-                    style: TextStyle(color: Colors.black,fontSize:14)
+                    right: 40,
                   ),
-                  TextSpan(
-                    text: "Login",
-                    style: TextStyle(color: Colors.white,fontSize: 14),
-                    recognizer: TapGestureRecognizer()..onTap=(){
-                     Navigator.pushReplacement(context, MaterialPageRoute(builder:(context) => Login(),));
-          
-                    }
-                  ),
-              ]
-            ))
-            // 
-          
-          
-            
-            )]
-             ),
-          ), 
-           
-       
-         
+                ),
+              );
+            } catch (e) {
+              print('Error adding data to Firestore: $e');
+              // Handle error appropriately
+            }
+          }
+        },
+      ),
+    );
+  }
+
+  Widget buildRichTextForLogin() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+      child: RichText(
+        text: TextSpan(
+          children: [
+            const TextSpan(
+              text: "Already Have An Account? ",
+              style: TextStyle(color: Colors.black, fontSize: 14),
+            ),
+            TextSpan(
+              text: "Login",
+              style: TextStyle(color: Colors.white, fontSize: 14),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Login()));
+                },
+            ),
+          ],
         ),
-        
-     )
-   );
+      ),
+    );
+  }
+
+  Future<void> addDataToFirestore(String email, String phone, String pass, String user) async {
+    CollectionReference registerReference = FirebaseFirestore.instance.collection('Students');
+
+    await registerReference.add({'email': email, 'phone': phone, 'password': pass, 'username': user});
   }
 }
