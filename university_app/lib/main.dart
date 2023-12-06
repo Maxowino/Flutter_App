b73b55b659f9c01019be9e47d8d211d2960aa943
Maxowino <maxwellowino27@gmail.com>
2023-12-06 16:36:20 +0300

    firestore function

--- university_app/lib/main.dart
+++ university_app/lib/main.dart
@@ -4,35 +4,36 @@ import 'package:university_app/screens/splashcreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-
-
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp(
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
-Future<void>addDataToFirestore(
-    email, phone,pass,user
-  )async{
-    CollectionReference registerReference=FirebaseFirestore.instance.collection('Students');
+  );
+  // Call the function to add data to Firestore
+  await addDataToFirestore('email@gmail.com', '2345654321', 'password', 'username');
+  runApp(MyApp());
+}
 
-    await registerReference.add({'email':'email@gmail.com','phone':'2345654321','password':'password','username':'username'});
-    
-  }
-  runApp( MyApp());
+Future<void> addDataToFirestore(email, phone, pass, user) async {
+  CollectionReference registerReference = FirebaseFirestore.instance.collection('Students');
+
+  await registerReference.add({
+    'email': email,
+    'phone': phone,
+    'password': pass,
+    'username': user,
+  });
 }
 
 class MyApp extends StatelessWidget {
-   MyApp({super.key});
+  MyApp({super.key});
 
- 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        title: "App",
-        debugShowCheckedModeBanner:false,
-       home:splashscreen(),
+      title: "App",
+      debugShowCheckedModeBanner: false,
+      home: splashscreen(),
     );
   }
 }
