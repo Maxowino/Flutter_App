33f05940738fadbe74947980d673ba4f982eb6a0
Maxowino <maxwellowino27@gmail.com>
2023-12-08 13:53:35 +0300

    profile controller

--- university_app/lib/controllers/profilecontroller.dart
+++ university_app/lib/controllers/profilecontroller.dart
@@ -1,11 +1,11 @@
 import 'package:get/get.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 
 class profilecontroller extends GetxController{
   static profilecontroller get instance=>Get.find();
 
 
-  get userData() {
+  // get userData() {
 
-  }
+  // }
 }
